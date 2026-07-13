16866ff9d3913fc3ccb6c7d7b8a36ae8febc86cc
jit0341 <jitendrablog6@gmail.com>
2026-07-13 21:00:21 +0530

    Webhook save payment

--- app/api/webhook/razorpay/route.ts
+++ app/api/webhook/razorpay/route.ts
@@ -15,7 +15,15 @@ function verifySignature(body: string, signature: string) {
 export async function POST(req: NextRequest) {
 
     try {
+         // Check Environment Variables
+        if (!WEBHOOK_SECRET) {
+            throw new Error("RAZORPAY_WEBHOOK_SECRET missing");
+        }
 
+        if (!SCRIPT_URL) {
+            throw new Error("GOOGLE_SCRIPT_URL missing");
+        }
+        
         const body = await req.text();
 
         const signature =
@@ -36,12 +44,59 @@ export async function POST(req: NextRequest) {
         }
 
         const event = JSON.parse(body);
+        // Ignore all other events
+if (event.event !== "payment.captured") {
+    return NextResponse.json({
+        success: true,
+        message: "Ignored"
+    });
+}
+
+const payment = event.payload.payment.entity;
+
+const gsResponse = await fetch(SCRIPT_URL, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        action: "SAVE_PAYMENT",
+        paymentId: payment.id,
+        orderId: payment.order_id || "",
+        customerName:
+            payment.customer_details?.name || "",
+        company: "",
+        email:
+            payment.email ||
+            payment.customer_details?.email ||
+            "",
+        mobile:
+            payment.contact ||
+            payment.customer_details?.contact ||
+            "",
+        plan: "STANDARD",
+        amount: payment.amount / 100
+    })
+});
+
+let gsResult = {};
+
+try {
+    gsResult = await gsResponse.json();
+} catch (e) {
+    console.error("Apps Script Response Error", e);
+}
+
+console.log(gsResult);
+
 
         console.log("Webhook Verified");
 
         return NextResponse.json({
-            success: true
-        });
+    success: true,
+    google: gsResult
+});
+
 
     }
 
